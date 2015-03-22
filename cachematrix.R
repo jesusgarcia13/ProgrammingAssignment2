5393927340ef7a8638c6965276466bb5d595ee4c
Jesus Garcia <jesusgarcia13@users.noreply.github.com>
2015-03-22 16:11:31 -0400

    Update cachematrix.R
    Created and unit-tested cached inverse matrix functions for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix: function to Cache Inverse matrix operation
+## Includes internal functions to handle cached inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	# Create empty Inverse matrix holder
+	ix <- matrix(, nrow=nrow(x),ncol=ncol(x))
+	
+	# setter, assign matrix and initialize ix
+	set <- function(y) {
+                x <<- y
+               ix <<- matrix(, nrow=nrow(x),ncol=ncol(x))
+        }
+    # getter, retrieve matrix
+	get <- function() x
+	
+	# Assign inverse matrix
+	setInverseMatrix <- function(imx) ix <<- imx
+    
+	# Retrieve inverse matrix
+    getInverseMatrix <- function() ix
+    
+	#list of internal functions
+    list(set = set, get = get,
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve: function to solve and 'cache' inverse matrix for
+## function makeCacheMatrix
+## Returns: a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ix <- x$getInverseMatrix()
+		# Check for 'empty' inverse matrix, if not, use cached value
+        if(!all(is.na(as.vector(ix)))) {
+                message("getting cached Inverse matrix")
+                return(ix)
+        }
+		# If not cached (first call), calculate or 'solve' the
+		# Inverse matrix and set the value using internal function
+		# setInverseMatrix
+        mdata <- x$get()
+        ix <- solve(mdata, ...)
+        x$setInverseMatrix(ix)  
+		# Return inverse matrix
+        ix
 }
